e8cf476354cf85f196baf5e1e89ca6ece351f530
Randy LeVeque <rjl@uw.edu>
2024-09-17 15:16:44 -0700

    add scripts for 3D pyvista animations

---
+++ geoclaw_run/pyvista_fgout_Nuu_debris.py
@@ -0,0 +1,203 @@
+from pylab import *
+import os
+import pyvista as pv
+from clawpack.geoclaw import fgout_tools
+import debris_tools
+from clawpack.visclaw import animation_tools
+from pyvista_nuu_debris import compute_debris_paths
+
+global etamesh, debris_spheres
+
+fgno = 5  # which fgout grid
+
+mstr = '10'
+outdir = '_output_%sm' % mstr
+format = 'binary32'  # format of fgout grid output
+qmap = 'geoclaw-bouss'  # defines mapping for fgout variables
+
+
+# where to save a png file for each frame, for constructing an animation:
+framedir = '_frames_%sm' % mstr
+os.system('mkdir -p %s' % framedir)
+os.system('rm %s/*' % framedir)  # remove frames from previous version
+
+tfinal = 15*60.  # create animation up to this time (or end of computation)
+
+# compute debris paths based on fgout2:
+debris_paths, dbnos_water, dbnos_land = compute_debris_paths(mstr,tfinal)
+
+debris_spheres = len(debris_paths)*[' ']  # for plot actors
+
+# Instantiate object for reading fgout frames:
+fgout_grid = fgout_tools.FGoutGrid(fgno, outdir, format, qmap) 
+#fgout_grid.read_fgout_grids_data_pre511(fgno)
+try:
+    fgout_grid.read_fgout_grids_data(fgno)
+except:
+    fgout_grid.read_fgout_grids_data_pre511(fgno)
+    fgout_grid.times = linspace(fgout_grid.tstart, fgout_grid.tend,
+                                fgout_grid.nout)
+fgout = fgout_grid.read_frame(1)
+
+# convert x,y to meters, roughly:
+
+x = (fgout.x - fgout.x[0]) * 111e3 * cos(fgout.y.mean()*pi/180)
+y = -(fgout.y - fgout.y[0]) * 111e3
+z = array([0.])
+X,Y,Z = meshgrid(x, y, z, indexing='ij')
+topoxyz = pv.StructuredGrid(X,Y,Z)
+
+B = fgout.B
+B = flipud(B)
+Bmax = 25.
+B = minimum(B, Bmax)
+topoxyz.point_data['B'] = B.flatten(order='F')
+
+warpfactor = 8  # amplification of elevations
+
+topowarp = topoxyz.warp_by_scalar('B', factor=warpfactor)
+
+texture = pv.read_texture('./GE_Nuu.jpg')
+GE_extent = fgout.extent_edges
+x2,x1 = (asarray(GE_extent[:2]) - fgout.x[0]) * 111e3 * cos(fgout.y.mean()*pi/180)
+y2,y1 = (asarray(GE_extent[2:]) - fgout.y[0]) * 111e3
+
+#x1,x2,y1,y2 = GE_extent
+
+o = (x1, y1, 0.)
+u = (x2, y1, 0.)
+v = (x1, y2, 0.)
+
+mapped_surf = topowarp.texture_map_to_plane(o, u, v)
+
+if 0:
+    # plot topo flat:
+    p = pv.Plotter()
+    p.add_mesh(topoxyz,cmap='gist_earth',clim=(-20,10))
+    p.view_xy()
+    p.show(window_size=(1500,1500))
+
+if 0:
+    # plot topo alone:
+    p = pv.Plotter()
+    p.add_mesh(topowarp,cmap='gist_earth',clim=(-20,20))
+    p.camera_position = 'xz'
+    p.camera.azimuth = 180
+    p.camera.elevation = 20
+    p.show(window_size=(1500,1500))
+
+# replace land surface by nan in eta so it only shows water:
+eta = where(fgout.h>0.1, fgout.eta, nan)
+eta = flipud(eta)
+
+make_animation = True
+
+# select frames for fgout grid 5 every 10 seconds up to tfinal:
+fgframes = [n+1 for n in range(len(fgout_grid.times)) \
+               if ((fgout_grid.times[n] <= tfinal) and \
+                  (mod(fgout_grid.times[n], 10) == 0))]
+
+p = pv.Plotter(off_screen=make_animation, lighting='three lights')
+
+#p.add_mesh(topowarp,cmap='gist_earth',clim=(-20,20))
+p.add_mesh(mapped_surf,texture=texture)
+
+topoxyz.point_data['eta'] = eta.flatten(order='F')
+etawarp = topoxyz.warp_by_scalar('eta', factor=warpfactor)
+etamesh = p.add_mesh(etawarp,color='c')
+
+if 0:
+    p.camera_position = 'xz'
+    p.camera.azimuth = 120
+    p.camera.elevation = 40
+    
+camera_position1 =  [(2351.473476407906, 223.43232607508872, 1290.8736572490081),
+ (387.9651184082031, -442.28570556640625, -49.22932434082031),
+ (-0.4995685365943228, -0.21553276761215126, 0.8390333147917629)]
+ 
+p.camera_position = camera_position1
+
+p.window_size=(1500,1500)
+
+
+def set_frameno(fgframeno):
+    global etamesh, debris_spheres
+    #fgframeno = 6*int(floor(tmin)) + 1
+    fgframeno = int(round(fgframeno))
+    fgout = fgout_grid.read_frame(fgframeno)
+    thour, remainder = divmod(fgout.t, 3600)
+    tmin, tsec = divmod(remainder, 60)
+    tstr = '%s:%s:%s' % (str(int(thour)).zfill(2),
+                      str(int(tmin)).zfill(2),
+                      str(int(tsec)).zfill(2))
+    print('Frame %i, t = %s' % (fgframeno, tstr))
+
+    eta = where(fgout.h>0.1, fgout.eta, nan)
+    eta = flipud(eta)
+    topoxyz.point_data['eta'] = eta.flatten(order='F')
+    etawarp = topoxyz.warp_by_scalar('eta', factor=warpfactor)
+    p.remove_actor(etamesh)
+    etamesh = p.add_mesh(etawarp,color='c')
+    
+    p.add_title('Time %s\nSlump amplitude %s m' % (tstr,mstr))
+
+    # add debris particles at this time:
+    xd_water,yd_water = debris_tools.get_debris_xy(fgout.t, debris_paths,
+                                                   dbnos_water)
+    xd_land,yd_land = debris_tools.get_debris_xy(fgout.t, debris_paths, 
+                                                   dbnos_land)
+    # make function that returns eta(x,y) at this time:
+    eta_fcn = fgout_tools.make_fgout_fcn_xy(fgout, 'eta')
+    B_fcn = fgout_tools.make_fgout_fcn_xy(fgout, 'B')
+    
+    
+    for k in range(len(xd_water)):
+        xd = -(xd_water[k]- fgout.x[-1]) * 111e3 * cos(fgout.y.mean()*pi/180)
+        yd = -(yd_water[k]- fgout.y[0]) * 111e3
+        sphere_radius = 5    
+        zd = warpfactor * max(eta_fcn(xd_water[k],yd_water[k]),
+                 B_fcn(xd_water[k],yd_water[k])+sphere_radius/warpfactor)
+        debris_sphere = pv.Sphere(radius=sphere_radius, center=(xd,yd,zd))
+        p.remove_actor(debris_spheres[k])
+        debris_spheres[k] = p.add_mesh(debris_sphere, color='r')
+        #print('Added sphere at (%.3f, %.3f, %.3f)' % (xd,yd,zd))
+        
+    if not make_animation:
+        # print camera position so that this can be copied and pasted
+        # into this script after adjusting (and then sliding frameno)
+        print('p.camera_position = ', p.camera_position)                   
+    
+        
+    # to capture each frame after moving slider, uncomment this:
+    #p.screenshot('PyVista_Nuu_Frame%s.png' % str(fgframeno).zfill(4))
+    
+#p.add_slider_widget(set_frameno, [0,20], value=0, title='Time (minutes)')
+#p.add_slider_widget(set_frameno, [0,4], value=0, title='Time (minutes)')
+
+
+
+
+if not make_animation:
+    frames_range = [fgframes[0], fgframes[-1]]
+    p.add_slider_widget(set_frameno, frames_range, value=1, title='Frameno',
+                    pointa=(0.4,0.85), pointb=(0.9,0.85), color='blue',
+                    slider_width=0.02, tube_width=0.005)
+
+    p.show()
+    
+else:
+
+    #for fgframeno in range(1,121,1):
+    #for fgframeno in range(1,74,1):
+    for fgframeno in fgframes:
+        set_frameno(fgframeno)
+        fname_png = '%s/PyVistaFrame%s.png' % (framedir, str(fgframeno).zfill(4))
+        p.screenshot(fname_png)
+        print('Created ',fname_png)
+        
+    p.close()
+
+    anim = animation_tools.make_anim(framedir, fname_pattern='PyVistaFrame*.png')
+    fname_mp4 = 'Nuu_debris_animation%sm_10sec.mp4' % mstr
+    animation_tools.make_mp4(anim, fname_mp4)
+    print('Created ',fname_mp4)
---
+++ geoclaw_run/pyvista_nuu_debris.py
@@ -0,0 +1,127 @@
+from pylab import *
+
+def compute_debris_paths(mstr, tfinal):
+
+    import debris_tools
+    #from clawpack.geoclaw import debris_tools
+    from clawpack.geoclaw import fgout_tools
+    from scipy.interpolate import interp1d
+
+    outdir = '_output_%sm' % mstr
+    output_format = 'binary32'
+    qmap = 'geoclaw-bouss'  # defines mapping for fgout variables
+
+    fgno = 5
+    fgout_grid2 = fgout_tools.FGoutGrid(fgno, outdir, output_format, qmap)
+    #fgout_grid2.read_fgout_grids_data(fgno)
+    #fgout_grid2.read_fgout_grids_data_pre511(fgno)
+    try:
+        fgout_grid2.read_fgout_grids_data(fgno)
+    except:
+        fgout_grid2.read_fgout_grids_data_pre511(fgno)
+        fgout_grid2.times = linspace(fgout_grid2.tstart, fgout_grid2.tend,
+                                    fgout_grid2.nout)
+
+    print('Looking for output in ',outdir)
+
+    # use all frames up to time tfinal:
+    fgframes2 = [n+1 for n in range(len(fgout_grid2.times)) \
+                   if fgout_grid2.times[n] <= tfinal]
+                   
+    print('In nuu_debris, fgout grid 5, using %i frames up to frame %i, t = %i sec' \
+        % (len(fgframes2), fgframes2[-1], fgout_grid2.times[fgframes2[-1]-1]))    
+                      
+    fgframe2 = fgframes2[0] # initial frame for fgout grid 2 
+    fgout2 = fgout_grid2.read_frame(fgframe2)
+
+    # Deterime time t of first fgout frame, to initialize particles
+    t0 = fgout2.t
+    fgout_extent = fgout2.extent_edges
+
+    # initial topography (used to determine land / water points):
+    # assumed to be same as B in fgout frames (no subsidence)
+    B0_fcn = fgout_tools.make_fgout_fcn_xy(fgout2, 'B')
+
+    # define beach region for placing initial particles only on beach:
+    
+    xy_beach = array([(-160, 20.62695),
+                      (-156.1804431818182, 20.62694805194805),
+                      (-156.17894318181817, 20.626974025974025),
+                      (-156.17794967532467, 20.626603896103894),
+                      (-156.1774237012987, 20.62638961038961),
+                      (-156.175, 20.62639)])
+     
+    ybeach = interp1d(xy_beach[:,0], xy_beach[:,1], 
+                       kind='linear', bounds_error=False, fill_value=nan)
+                       
+    # Initialize debris_paths dictionary and set
+    # initial debris particle locations (x0,y0) at time t0.
+    # Require a list of dbnos and each array 
+    #     debris_paths[dbno]
+    # in the dictionary is a 2d array with a single row [t0, x0, y0] to start.
+
+    debris_paths = {}
+    dbnos_water = []
+    dbnos_land = []
+
+    # same for all debris particles:
+    grounding_depth = 0.1
+    grounding_speed = 5.
+    #drag_factor = None
+    drag_factor = 10.
+            
+    # set initial velocities to 0 (or may want to interpolate from fgout0 if t0>0?)
+    u0 = 0.
+    v0 = 0.
+
+    x1,x2 = -156.181, -156.177
+    y1,y2 = 20.625, 20.6273
+    #x1,x2 = -156.180, -156.179
+    #y1,y2 = 20.625, 20.626
+    dxd = 0.0002
+
+    dyd = dxd * cos(21*pi/180)
+    xd = arange(x1, x2, dxd)
+    yd = arange(y1, y2, dyd)
+    mxd = len(xd)
+    myd = len(yd)
+    print('will sample array of %i by %i points for particles' \
+            % (mxd,myd))
+
+    for i in range(len(xd)):
+        x0 = xd[i]
+        for j in range(len(yd)):
+            y0 = yd[j]
+            B0xy = B0_fcn(x0,y0)
+            #if B0xy < -10 or B0xy > 5:
+            if B0xy < -5 or B0xy > 4:
+                continue # skip point unless near shore
+            if x0 > -156.1778 and y0 > 20.6265:
+                #continue # skip point if in pond
+                pass
+            if y0 > 20.6273:
+                continue # skip points inland
+            if y0 > ybeach(x0):
+                continue # skip points too far inland
+            dbno = 1000*i + j
+            db = array([[t0, x0, y0, u0, v0]])
+            debris_paths[dbno] = db
+            
+            if B0xy < 0:
+                dbnos_water.append(dbno)
+            elif B0xy > 0:
+                dbnos_land.append(dbno)
+
+    dbnos = dbnos_water + dbnos_land
+    print('Created %i initial debris particles' % len(dbnos))
+    print('    %i offshore,  %i onshore' % (len(dbnos_water),len(dbnos_land)))
+
+    # Compute debris path for each particle by using all the fgout frames
+    # in the list fgframes (first frame should be one used to set t0 above):
+
+    debris_paths = debris_tools.make_debris_paths(fgframes2, fgout_grid2, 
+                                debris_paths, dbnos, drag_factor, grounding_depth, grounding_speed)
+
+    # done computing debris paths
+    # ------------------------------
+    return debris_paths, dbnos_water, dbnos_land
